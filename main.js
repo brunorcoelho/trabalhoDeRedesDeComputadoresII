ae8ae2e43b89cf45fce2aa01c18988f2ebfc0ad8
Bruno Coelho <bruno090904@gmail.com>
2025-08-17 17:20:44 -0400

    add: connection to postgres

---
+++ main.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const {Pool} = require('pg');
+const { database, password } = require('pg/lib/defaults');
+
+const app = express()
+const port = 3000;
+
+const pool = new Pool ({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+})
+
+app.get('/', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        res.send('<h1>Conexão com o banco de dados PostgreSQL realizada com sucesso.</h1>');
+        client.release();
+    }
+    catch (err) {
+        res.status(500).send('<h1>Conexão com o banco de dados PostgreSQL mal-sucedida. Erro: </h1><pre>' + err.stack)
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Aplicação rodando em http://localhost:${port}`)
+})
